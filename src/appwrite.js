c6d68eeeaac2da2165faaa4c337c58187229c9f0
sharkny <sharknystudio@gmail.com>
2025-11-03 21:23:53 +0900

    bug fixed

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Databases, Query } from "appwrite";
+import { Client, Databases, Query, ID } from "appwrite";
 
 const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 const PROJECT_ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
@@ -7,35 +7,50 @@ const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
 const client = new Client()
     .setEndpoint(PROJECT_ENDPOINT)
-    .setProject(PROJECT_ID)
-
+    .setProject(PROJECT_ID);
 
 const database = new Databases(client);
 
-
 export const updateSearchCount = async (searchTerm, movie) => {
     try {
+        // 1. Aynı searchTerm ile belge ara (tam eşleşme)
         const result = await database.listDocuments(
             DATABASE_ID,
             COLLECTION_ID,
-            [Query.search('searchTerm', searchTerm)]
-
-        if(result.documents.length > 0)
-        {
-            const doc = result.documents[0];
-
-            await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id,
-                {count: doc.count = 1,}
-            )
-        }
-        else
-        {
-            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(),
-                {searchTerm, count: 1, movie_id: movie.id, poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path},`}
-            )
-        }
+            [
+                Query.equal('searchTerm', searchTerm),
+                Query.limit(1)
+            ]
         );
-    } catch (error){
-        console.log(error);
+
+        if (result.documents.length > 0) {
+            const doc = result.documents[0];
+            // 2. Sayacı 1 artır
+            await database.updateDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                doc.$id,
+                {
+                    count: doc.count + 1
+                }
+            );
+        } else {
+            // 3. Yeni belge oluştur
+            await database.createDocument(
+                DATABASE_ID,
+                COLLECTION_ID,
+                ID.unique(),
+                {
+                    searchTerm: searchTerm,
+                    count: 1,
+                    movie_id: movie.id,
+                    poster_url: movie.poster_path 
+                        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` 
+                        : null
+                }
+            );
+        }
+    } catch (error) {
+        console.error("Appwrite search count update error:", error);
     }
-}
+};
